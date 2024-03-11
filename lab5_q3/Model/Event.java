097a991745c4c3a5f2ba05d144db6c99bdad5713
Ghadamsm <68555859+Ghadamsm@users.noreply.github.com>
2024-03-11 16:02:07 +0300

    Add files via upload

--- lab5_q3/Model/Event.java
+++ lab5_q3/Model/Event.java
@@ -1,10 +1,7 @@
 package com.example.lab5_q3.Model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -27,16 +24,15 @@ public class Event {
     private String description ;
 
 
-@NotEmpty(message = "capacity should be not empty")
+@NotNull(message = "capacity should be not empty")
 @Min(value = 25 , message = "too short")
-@Pattern(regexp = "^[0-9]+(\\.[0-9]+)?$", message = "please enter a numeric value")
     private Integer capacity;
 
 
 //    @JsonFormat(pattern = "yyyy-MM-dd")
 //    private int startDate;
 //
-//    @JsonFormat(pattern = "hh-mm")
+//    @JsonFormat(pattern = "yyyy-MM-dd")
 //    LocalDate endTime ;
 
 
